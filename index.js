f9702c4bc850401bb46f48bbe23ac05ed6751e9e
Алексей Сидоряк <alexeys1doryak@gmail.com>
2023-05-16 12:59:17 +0300

    root vs. node

--- index.js
+++ index.js
@@ -4,22 +4,16 @@ const bip39 = require('bip39')
 const bitcoin = require('bitcoinjs-lib')
 const qrcode = require('qrcode-terminal')
 
-//Define the network
-// const network = bitcoin.networks.bitcoin //use networks.testnet for testnet
-
 //Derivation path
-// const path = `m/44'/0'/0'/0`
+const path = `m/44'/0'/0'/0`
 
 let mnemonic = bip39.generateMnemonic()
 
-// const seed = bip39.mnemonicToSeedSync(mnemonic)
+const seed = bip39.mnemonicToSeedSync(mnemonic)
 
-let root = bip32.fromSeed(
-    bip39.mnemonicToSeedSync(mnemonic)
-)
+let root = bip32.fromSeed(seed)
 
-// let account = root.derivePath(path)
-// let node = account.derive(0).derive(0)
+let node = root.derivePath(path).derive(0).derive(0) // TODO: root vs. node
 
 let wallet = bitcoin.payments.p2pkh({
   pubkey: root.publicKey // TODO: root vs. node
@@ -32,6 +26,7 @@ qrcode.generate(secret, {small: false}, function (qrcode) {
     console.log(`
     BTC:
     - Address  : ${wallet.address}
+    - WIF      : ${secret}
     - Mnemonic : ${mnemonic}
     `)
 })
