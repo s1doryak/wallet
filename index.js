8a5fd42d8f98606ac01860674ff06e7c940b632b
Алексей Сидоряк <132191647+s1doryak@users.noreply.github.com>
2024-06-20 16:42:46 +0300

    bip39 passphrase

--- index.js
+++ index.js
@@ -1,28 +1,34 @@
 // References:
 // - https://learnmeabitcoin.com/technical/keys/hd-wallets/derivation-paths/
 // - https://github.com/bitcoinjs/bitcoinjs-lib/blob/master/test/integration/addresses.spec.ts
+// - https://www.npmjs.com/package/bip39
+
 const bip32 = require('bip32')
 const bip39 = require('bip39')
 const bitcoin = require('bitcoinjs-lib')
 const qrcode = require('qrcode-terminal')
 
-const mnemonic = bip39.generateMnemonic()
+const words = process.argv.slice(2)
 
-const seed = bip39.mnemonicToSeedSync(mnemonic)
+const mnemonic = words.length ? words.slice(0, 12).join(' ') : bip39.generateMnemonic()
+
+const passphrase = words.length ? words.slice(12, 1).toString() : ''
+
+const seed = bip39.mnemonicToSeedSync(mnemonic, passphrase)
 
 const root = bip32.fromSeed(seed)
 
-// P2PKH (Legacy) - m/44'/0'/0'/0/0
+// P2PKH (Legacy)
 const wallet1 = bitcoin.payments.p2pkh({
   pubkey: root.derivePath(`m/44'/0'/0'/0/0`).publicKey
 })
 
-// P2SH (SegWit compatible) - m/49'/0'/0'/0/0
+// P2SH (SegWit compatible)
 const wallet2 = bitcoin.payments.p2sh({ redeem: bitcoin.payments.p2wpkh({ 
   pubkey: root.derivePath(`m/49'/0'/0'/0/0`).publicKey
 })})
 
-// Bech32 (SegWit native) - m/84'/0'/0'/0/0
+// Bech32 (SegWit native)
 const wallet3 = bitcoin.payments.p2wpkh({ 
   pubkey: root.derivePath(`m/84'/0'/0'/0/0`).publicKey
 })
